9da3515ff5ee51d743561c8b4952dbaa3085ca64
thieubaviett04 <tbviet2004@gmail.com>
2026-06-23 21:36:59 +0700

    Validation DTO

--- TodoAPI/DTOs/UpdateItemDto.cs
+++ TodoAPI/DTOs/UpdateItemDto.cs
@@ -1,9 +1,14 @@
-﻿namespace TodoAPI.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+namespace TodoAPI.DTOs
 {
     public class UpdateItemDto
     {
+        [Required(ErrorMessage = "Tên công việc không được để trống")]
+        [StringLength(100, ErrorMessage = "Tên công việc không được quá 100 ký tự")]
         public string? Name { get; set; }
         public bool IsComplete { get; set; }
+
+        [StringLength(500, ErrorMessage = "Mô tả công việc không được quá 500 ký tự")]
         public string? Description { get; set; }
     }
 }
